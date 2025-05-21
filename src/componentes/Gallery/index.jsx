17c334ada414a10dec69b31c462615ed6882952b
enzirneo <enzo.bispo22@gmail.com>
2025-05-21 15:20:39 -0300

    Ajustando a responsividade

--- src/componentes/Gallery/index.jsx
+++ src/componentes/Gallery/index.jsx
@@ -3,7 +3,7 @@ import PhotoCard from "../PhotoCard";
 
 export default function Gallery({ photos, onPhotoClick, isDark, favorites, toggleFavorite }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(220px,1fr))] gap-6 px-4">
       {photos.map((photo) => (
         <PhotoCard
           key={photo.id}
